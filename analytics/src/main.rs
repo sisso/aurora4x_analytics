4033a8ce50148494614e420256f3c608536df8d1
sisso <sissobr@gmail.com>
2020-09-25 22:25:18 +0200

    adding webserver and basic javascript

--- analytics/src/main.rs
+++ analytics/src/main.rs
@@ -1,3 +1,25 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::content;
+use serde_json::json;
+use std::fs::File;
+use std::io::Read;
+
+#[get("/")]
+fn index() -> Result<content::Html<String>, std::io::Error> {
+    let body = std::fs::read_to_string("analytics/resources/index.html")?;
+    Ok(content::Html(body))
+}
+
+#[get("/data")]
+fn data() -> Result<content::Json<String>, Box<dyn std::error::Error>> {
+    let body = std::fs::read_to_string("data/dashboard_data.json")?;
+    Ok(content::Json(body))
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index, data]).launch();
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # Aurora 4x Analytics
 
 A simple code that dump the game data into a file every time the database is saved to generate historical data
+
+# TODO
+
+- add population name to the files
+- historical data should be sorted by time
+- update html to select game/population
